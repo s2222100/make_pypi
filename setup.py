6f5a0ab24072b0ab191ca7196c32defe94864112
s2222100 <116871814+s2222100@users.noreply.github.com>
2024-05-27 02:08:09 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open('README.md', 'r', encoding='utf-8')as fh:
   long_description = fh.read()
 setuptools.setup(
   name='original_janken',
-  version='0.01',
+  version='0.0.1',
   author='Takeru_Wakatsuki',
   author_email='s2222100@stu.musashino-u.ac.jp',
   description='You can make original rock paper scissors.',
@@ -11,7 +11,7 @@ setuptools.setup(
   long_description_content_type='text/markdown',
   url='https://github.com/s2222100/orijinal_janken',
   project_urls={
-    "Bug Tracker":"https://github.com/s2222100/orijinal_janken"
+    "Bug Tracker":"https://github.com/s2222100/original_janken"
   },
   classifiers=[
     'Programming Language :: Python :: 3',
@@ -25,7 +25,7 @@ setuptools.setup(
   entry_points={
     'console_points={
       'console_scripts':[
-        'orijinal_janken=orijinal_janken:main'
+        'original_janken=original_janken:main'
     ]
   },
 )
