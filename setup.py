e1f997937cbcf08fa331b9e7b390bbc303c59516
s2222100 <116871814+s2222100@users.noreply.github.com>
2024-05-27 02:00:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,9 @@ setuptools.setup(
   description='You can make original rock paper scissors.',
   long_description=long_description,
   long_description_content_type='text/markdown',
-  #
-  url='https://github.com/s2222100/make_pypi',
-  #
+  url='https://github.com/s2222100/orijinal_janken',
   project_urls={
-    "Bug Tracker":"https://github.com/s2222100/make_pypi"
+    "Bug Tracker":"https://github.com/s2222100/orijinal_janken"
   },
   classifiers=[
     'Programming Language :: Python :: 3',
@@ -21,15 +19,13 @@ setuptools.setup(
     'Operating System :: OS Independent',
   ],
   package_dir={'':'src'},
-  #
-  py_modules=["good_name"],
+  py_modules=["original_janken"],
   packages=setuptools.find_package(where='src'),
   python_requires='>=3.7',
   entry_points={
     'console_points={
       'console_scripts':[
-      #
-        'name=name:main'
+        'orijinal_janken=orijinal_janken:main'
     ]
   },
 )
